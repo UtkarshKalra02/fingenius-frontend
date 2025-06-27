a9ebcbd421b13406abd7bbae4b46335a8eaf70a2
Utkarsh Kalra <utkarshkalra02@gmail.com>
2025-06-27 14:08:11 +0530

    localhost changes

--- src/utils/oidconfig.js
+++ src/utils/oidconfig.js
@@ -1,8 +1,8 @@
 export const oidConfig = {
     authority: "https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_DrElqz2Cd",
     client_id: "5rckvpl3780cids2uafeljdl73",
-    redirect_uri: "http://localhost:3000/auth/callback",
-    post_logout_redirect_uri: "http://localhost:3000",
+    redirect_uri: `${window.location.origin}/auth/callback`,
+    post_logout_redirect_uri: `${window.location.origin}`,
     response_type: "code",
     scope: "openid email phone",
 }; 
